d57279b890b07ecd46caba764d804b7052034dfd
maellia-giulia.cabon <maellia-giulia.cabon@isen.yncrea.Fr>
2022-03-07 18:32:35 +0100

    third commit

--- app/src/main/java/com/example/androiderestaurant/CategoryActivity.kt
+++ app/src/main/java/com/example/androiderestaurant/CategoryActivity.kt
@@ -1,15 +1,17 @@
 package com.example.androiderestaurant
 
+import android.content.Intent
 import android.net.wifi.aware.AwareResources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class CategoryActivity : AppCompatActivity() {
-    lateinit var myRecycler : RecyclerView
+    lateinit var myRecycler: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,13 +19,22 @@ class CategoryActivity : AppCompatActivity() {
 
         val extra = intent.getStringExtra("12345")
         setTitle(extra)
-        val category :List<Category> = when (extra){
-            "Entrées"->{resources.getStringArray(R.array.starters).map {
-                Category(it) }}
-            "Plats"->{resources.getStringArray(R.array.dishes).map {
-                Category(it) }}
-            "Desserts"->{resources.getStringArray(R.array.deserts).map {
-                Category(it) }}
+        val category: List<Category> = when (extra) {
+            "Entrées" -> {
+                resources.getStringArray(R.array.starters).map {
+                    Category(it)
+                }
+            }
+            "Plats" -> {
+                resources.getStringArray(R.array.dishes).map {
+                    Category(it)
+                }
+            }
+            "Desserts" -> {
+                resources.getStringArray(R.array.deserts).map {
+                    Category(it)
+                }
+            }
             else -> {
                 emptyList()
             }
@@ -31,10 +42,14 @@ class CategoryActivity : AppCompatActivity() {
         myRecycler = findViewById(R.id.CategoryList)
         myRecycler.layoutManager = LinearLayoutManager(this)
         myRecycler.adapter = CategoryAdapter(category.toTypedArray()) {
-            Toast.makeText(this,"Vous avez selectionné ${it.nom}", Toast.LENGTH_SHORT).show()
+            startElement(it.nom)
         }
-
-
         val content = findViewById<RecyclerView>(R.id.CategoryList)
+    }
+
+    private fun startElement(message: String) {
+            val intent = Intent(this, ElementActivity::class.java).apply {
+                putExtra("12345", message)
+            }
     }
 }
