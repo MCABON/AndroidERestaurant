78db34ad0cc777357bbfddf20cb40a6343ef94b7
maellia-giulia.cabon <maellia-giulia.cabon@isen.yncrea.Fr>
2022-03-23 17:06:10 +0100

    third part + questions a and b part 4 done

--- app/src/main/java/com/example/androiderestaurant/ImageFragment.kt
+++ app/src/main/java/com/example/androiderestaurant/ImageFragment.kt
@@ -26,42 +26,4 @@ class ImageFragment(val url:String?) :Fragment() {
         super.onViewCreated(view, savedInstanceState)
         Picasso.get().load(url).placeholder(R.drawable.logodishes).into(binding.imageDishes)
     }
-
-
-
-
-
-
-
-
-
-
-
-
-   /* private var url: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            url = it.getString(ARG_PARAM)
-        }
-    }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        val view = inflater.inflate(R.layout.fragment_image, container, false)
-        url = url.takeIf { it != "" }
-        Picasso.get().load(url).placeholder(R.drawable.dishes).into(view.findViewById(R.id.imageDishes) as ImageView)
-        Log.d("CFOCV00", "$url" )
-        return view
-    }
-
-    companion object {
-        fun newInstance(url: String) =
-            ImageFragment().apply {
-                Log.d("CFOCV01", "$url" )
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM, url)
-                }
-            }
-    }*/
 }
